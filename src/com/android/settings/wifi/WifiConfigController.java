b675ccfcb29a87ad0ef2838e6d11464e2c9476bf
mickybart <mickybart@pygoscelis.org>
2015-04-09 16:13:57 -0400

    Revert "EAP-SIM and EAP-AKA support"
    This reverts commit 9bc8ddce2b1666802319d0376d5875149fc430c7.

--- src/com/android/settings/wifi/WifiConfigController.java
+++ src/com/android/settings/wifi/WifiConfigController.java
@@ -92,8 +92,6 @@ public class WifiConfigController implements TextWatcher,
     public static final int WIFI_EAP_METHOD_TLS  = 1;
     public static final int WIFI_EAP_METHOD_TTLS = 2;
     public static final int WIFI_EAP_METHOD_PWD  = 3;
-    public static final int WIFI_EAP_METHOD_SIM  = 4;
-    public static final int WIFI_EAP_METHOD_AKA  = 5;
 
     /* These values come from "wifi_peap_phase2_entries" resource array */
     public static final int WIFI_PEAP_PHASE2_NONE 	    = 0;
@@ -764,15 +762,6 @@ public class WifiConfigController implements TextWatcher,
                 mView.findViewById(R.id.l_anonymous).setVisibility(View.VISIBLE);
                 setUserCertInvisible();
                 break;
-            case WIFI_EAP_METHOD_SIM:
-            case WIFI_EAP_METHOD_AKA:
-                setPhase2Invisible();
-                setCaCertInvisible();
-                setAnonymousIdentInvisible();
-                setUserCertInvisible();
-                setPasswordInvisible();
-                setIdentityInvisible();
-                break;
         }
     }
 
@@ -800,11 +789,6 @@ public class WifiConfigController implements TextWatcher,
         mPasswordView.setText("");
         mView.findViewById(R.id.password_layout).setVisibility(View.GONE);
         mView.findViewById(R.id.show_password_layout).setVisibility(View.GONE);
-    }
-
-    private void setIdentityInvisible() {
-        mEapIdentityView.setText("");
-        mView.findViewById(R.id.l_identity).setVisibility(View.GONE);
     }
 
     private void showIpConfigFields() {
